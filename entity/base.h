b8056e622d51ae1aacb77ffa5244a4d1690edfe6
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-21 12:28:16 +0200

    add base class

---
+++ entity/base.h
@@ -0,0 +1,18 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include "entity.h"
+
+class base : protected entity
+{
+public:
+    base(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+        health = 200;
+        speed = 0;
+    };
+};
+
+#endif
