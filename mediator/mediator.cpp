ceda6bc04a9cff7b776842a91e20dd1e2817dfe1
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-21 23:47:08 +0200

    add exec_orders and any needed method - move and attack cases are done

--- mediator/mediator.cpp
+++ mediator/mediator.cpp
@@ -2,17 +2,236 @@
 
 mediator::mediator(const char *map, const char *status, const char *orders)
 {
+    turn = 1;
+    // remeber files names for reausage later on
+    file_names[0] = map;
+    file_names[1] = status;
+    file_names[2] = orders;
+
+    // open .txt files
     this->map.open(map);
     this->status.open(status);
-    this->orders.open(orders);
 
-    if (!this->map.is_open() || !this->status.is_open() || !this->orders.is_open())
+    if (!this->map.is_open() || !this->status.is_open())
     {
         std::cout << "Wrong input";
     }
+
+    // get map size and the obstacles' coordinates
+    std::string line;
+    while (getline(this->map, line))
+    {
+        for (int col = 0; col < line.length(); col++)
+        {
+            if (line[col] == '9')
+            {
+                obstacles.emplace_back(col, mapSize.second);
+            }
+        }
+        if (mapSize.first == 0)
+        {
+            mapSize.second = line.length();
+        }
+        mapSize.second++;
+    }
+
+    // set file flag to the beggining
+    this->map.clear();
+    this->map.seekg(0, std::ios::beg);
+
+    // set the bases
+    playerOneUnits.insert(std::make_pair(1, new base(0, 0)));
+    playerTwoUnits.insert(std::make_pair(1, new base(mapSize.first, mapSize.second)));
 }
 
 void mediator::update_map()
 {
+}
 
+void mediator::update_status()
+{
+}
+
+void mediator::exec_orders()
+{
+    // get the order line text and divide it into seperate words
+    std::string order;
+    while (getline(orders, order))
+    {
+        if (order.length() >= 3)
+        {
+            std::istringstream iss(order);
+            std::string word;
+
+            // order but word by word
+            std::vector<std::string> owbw;
+            while (iss >> word)
+            {
+                owbw.push_back(word);
+            }
+
+            // use player list based on which player's turn is now
+            auto &playerUnits = (turn) ? playerOneUnits : playerTwoUnits;
+            auto &enemyUnits = (turn) ? playerTwoUnits : playerOneUnits;
+
+            // get particular unit using the provided ID
+            // first check if a unit with that ID even exists to prevent any memory leaks
+            entity *unit;
+            if (playerUnits.find(std::stoi(owbw[0])) != playerUnits.end())
+            {
+                unit = playerUnits[std::stoi(owbw[0])];
+            }
+            else
+            {
+                throw "Unit with provided ID does not exists";
+            }
+
+            // perform an action based on given order
+            if (owbw[1] == "M")
+            {
+                // has to check if the space if occupied
+                if (can_move(std::stoi(owbw[2]), std::stoi(owbw[3])))
+                {
+                    if (!unit->move(std::stoi(owbw[2]), std::stoi(owbw[3])))
+                    {
+                        throw "Given move is not valid";
+                    };
+                }
+                else
+                {
+                    throw "Given move is not valid";
+                }
+            }
+            else if (owbw[1] == "A")
+            {
+                // check if an enemy unit with provided ID exists in the enemy unit structure
+                // doing that we do not have to check if we are attacking our own unit or an empty square
+                entity *enemyUnit;
+                if (enemyUnits.find(std::stoi(owbw[2])) != enemyUnits.end())
+                {
+                    enemyUnit = enemyUnits[std::stoi(owbw[2])];
+                }
+                else
+                {
+                    throw "Enemy unit with provided ID does not exists";
+                }
+
+                auto coordinates = enemyUnit->get_position();
+                if (unit->attack(coordinates.first, coordinates.second))
+                {
+                    enemyUnit->damage(fight(unit->get_type(), enemyUnit->get_type()));
+
+                    // if the attacked enemy's health is below or equal to 0, delete it
+                    if (enemyUnit->get_health() <= 0)
+                    {
+                        delete enemyUnit;
+                        enemyUnits.erase(enemyUnits.find(std::stoi(owbw[2])));
+                    }
+                }
+                else
+                {
+                    throw "Given attack is not valid";
+                }
+            }
+            else if (owbw[1] == "B")
+            {
+                
+            }
+            else
+            {
+                throw "Given order is not valid";
+            }
+        }
+    }
+}
+
+entity *mediator::create_entity(char unitType, int x, int y)
+{
+    entity *newUnit = nullptr;
+    switch (unitType)
+    {
+    case 'A':
+        newUnit = new archer(x, y);
+        break;
+    case 'C':
+        newUnit = new catapult(x, y);
+        break;
+    case 'K':
+        newUnit = new knight(x, y);
+        break;
+    case 'P':
+        newUnit = new pikeman(x, y);
+        break;
+    case 'R':
+        newUnit = new ram(x, y);
+        break;
+    case 'S':
+        newUnit = new swordsman(x, y);
+        break;
+    case 'W':
+        newUnit = new worker(x, y);
+        break;
+    }
+
+    return newUnit;
+}
+
+int mediator::fight(char attacker, char defender)
+{
+    int damage;
+    switch (attacker)
+    {
+    case 'A':
+        damage = (defender == 'C' || defender == 'R') ? 10 : 15;
+        break;
+    case 'C':
+        damage = (defender == 'B') ? 50 : 40;
+        break;
+    case 'K':
+        damage = (defender == 'R') ? 50 : 35;
+        break;
+    case 'P':
+        damage = (defender == 'B' || defender == 'R') ? 10 : 15;
+        damage = (defender == 'K') ? 35 : damage;
+        break;
+    case 'R':
+        damage = (defender == 'B') ? 50 : 10;
+        break;
+    case 'S':
+        damage = (defender == 'P' || defender == 'C') ? 20 : 30;
+        break;
+    case 'W':
+        damage = (defender == 'B') ? 1 : 5;
+        break;
+    }
+    return damage;
+}
+
+bool mediator::can_move(int x, int y)
+{
+    // can't move to a square occupied by enemy unit
+    auto &enemyUnits = (turn == 1) ? playerTwoUnits : playerOneUnits;
+    for (auto enemyUnit : enemyUnits)
+    {
+        auto coordinates = enemyUnit.second->get_position();
+        if (coordinates.first == x && coordinates.second == y)
+        {
+            return false;
+        }
+    }
+    // can't move to an obstacle square
+    for (auto obstacle : obstacles)
+    {
+        if (x == obstacle.first && y == obstacle.second)
+            return false;
+    }
+    // can't exit the map
+    if (x >= mapSize.first || y >= mapSize.second || x < 0 || y < 0)
+        return false;
+    // can't move to the enemy base square
+    auto enemyBaseCoords = enemyUnits[1]->get_position();
+    if (x == enemyBaseCoords.first && y == enemyBaseCoords.second)
+        return false;
+
+    return true;
 }
--- mediator/mediator.h
+++ mediator/mediator.h
@@ -4,6 +4,19 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "archer.h"
+#include "base.h"
+#include "catapult.h"
+#include "knight.h"
+#include "pikeman.h"
+#include "ram.h"
+#include "swordsman.h"
+#include "worker.h"
+#include "entity.h"
 
 class mediator
 {
@@ -13,11 +26,29 @@ public:
 private:
     // update map based on given orders
     void update_map();
+    // read the status file
+    void update_status();
+    // checks if the given orders are valid, and if so, executes them 
+    void exec_orders();
+    // create entity if needed
+    entity *create_entity(char unitType, int x, int y);
+    // checks how much damage a unit is dealing to diffrent unit
+    int fight(char attacker, char defender);
+    // checks if a unit can move to the given square (is it occupied)
+    bool can_move(int x, int y);
 
 private:
+    const char *file_names[3];
     std::fstream map;
     std::fstream status;
     std::fstream orders;
+    std::unordered_map<int, entity *> playerOneUnits;
+    std::unordered_map<int, entity *> playerTwoUnits;
+    // squares where a unit can't stay
+    std::vector<std::pair<int, int>> obstacles;
+    std::pair<int, int> mapSize{0, 0};
+    // 1 - first player / 0 - second player
+    bool turn;
 };
 
 #endif
