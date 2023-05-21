fa6303386768ce90544dd4248135867c3269ded1
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-21 12:21:10 +0200

    create archer class

---
+++ entity/archer.h
@@ -0,0 +1,23 @@
+#ifndef ARCHER_H
+#define ARCHER_H
+
+#include "entity.h"
+
+class archer : protected entity
+{
+public:
+    archer(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+        health = 40;
+        speed = 2;
+        cost = 250;
+        attackRange = 5;
+        buildTime = 3;
+        unitType = 'A';
+        attacked = false;
+    };
+};
+
+#endif
