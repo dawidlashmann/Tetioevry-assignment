3afa15d23df46bfee34d03e230d3cd0b8d051179
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-21 12:23:27 +0200

    create pikeman class

---
+++ entity/pikeman.h
@@ -0,0 +1,23 @@
+#ifndef PIKEMAN_H
+#define PIKEMAN_H
+
+#include "entity.h"
+
+class pikeman : protected entity
+{
+public:
+    pikeman(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+        health = 50;
+        speed = 2;
+        cost = 200;
+        attackRange = 2;
+        buildTime = 3;
+        unitType = 'P';
+        attacked = false;
+    };
+};
+
+#endif
