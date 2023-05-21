ad06d942928342de1f1dd00409101cc376d1fcc9
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-21 12:24:23 +0200

    add ram class

---
+++ entity/ram.h
@@ -0,0 +1,23 @@
+#ifndef RAM_H
+#define RAM_H
+
+#include "entity.h"
+
+class ram : protected entity
+{
+public:
+    ram(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+        health = 90;
+        speed = 2;
+        cost = 500;
+        attackRange = 1;
+        buildTime = 4;
+        unitType = 'R';
+        attacked = false;
+    };
+};
+
+#endif
