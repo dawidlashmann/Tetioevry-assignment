0c621f1ec4fea0fd3db507db6ede40980cc0c576
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-22 12:51:19 +0200

    fun with main_player.cpp

--- player/main_player.cpp
+++ player/main_player.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
 int main(int argc, char *argv[])
 {
+    const char *map, *status, *orders;
+    float limitCzasowy = 5.0f;
+    if (argc == 5)
+    {
+        const char *lim = argv[4];
+        limitCzasowy = std::stof(lim);
+    }
+    else if (argc != 4)
+    {
+        std::cout << "Wrong input\nExiting...\n";
+        return 1;
+    }
+    map = argv[1];
+    status = argv[2];
+    orders = argv[3];
+
     return 0;
 }
