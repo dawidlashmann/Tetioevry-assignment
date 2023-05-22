c425eaa4314e64e3fe9bf6e7c3399d9e2a7e8b6b
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-22 13:52:56 +0200

    add build directory

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dawid/repos/Tetioevry-assignment/entity/entity.cpp" "/home/dawid/repos/Tetioevry-assignment/build/CMakeFiles/game.dir/entity/entity.cpp.o"
+  "/home/dawid/repos/Tetioevry-assignment/mediator/main_mediator.cpp" "/home/dawid/repos/Tetioevry-assignment/build/CMakeFiles/game.dir/mediator/main_mediator.cpp.o"
+  "/home/dawid/repos/Tetioevry-assignment/mediator/mediator.cpp" "/home/dawid/repos/Tetioevry-assignment/build/CMakeFiles/game.dir/mediator/mediator.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../entity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/entity/entity.cpp.o"
+  "CMakeFiles/game.dir/mediator/main_mediator.cpp.o"
+  "CMakeFiles/game.dir/mediator/mediator.cpp.o"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
