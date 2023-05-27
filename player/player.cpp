edfaa03f31cea7d0238984eb272c1cadac059214
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-05-27 11:50:41 +0200

    develop move method

--- player/player.cpp
+++ player/player.cpp
@@ -86,6 +86,97 @@ player::~player()
     orders.close();
 }
 
+void player::get_orders()
+{
+    for (auto unit : ownUnits)
+    {
+        auto unitMove = move(unit.second);
+    }
+}
+
+std::pair<int, int> player::move(entity *unit)
+{
+    const std::pair<int, int> &unitCoords = unit->get_position();
+    int moveX, moveY;
+    int movesAvaiable = unit->get_speed() - 1;
+
+    // "best" move case
+    {
+        const std::pair<int, int> &enemyBaseCoords = enemyBase->get_position();
+
+        int distanceX = abs(unitCoords.first - enemyBaseCoords.first);
+        int distanceY = abs(unitCoords.second - enemyBaseCoords.second);
+        int distance = distanceX + distanceY;
+
+        float distanceRatioX = static_cast<float>(distanceX) / static_cast<float>(distance);
+        float distanceRatioY = static_cast<float>(distanceY) / static_cast<float>(distance);
+        moveX = static_cast<int>(distanceRatioX * movesAvaiable);
+        moveY = static_cast<int>(distanceRatioY * movesAvaiable);
+    }
+
+    std::vector<std::pair<int, std::pair<int, int>>> legalMoves;
+
+    std::queue<std::pair<int, int>> adjacentSquares;
+    adjacentSquares.emplace(unitCoords.first + moveX, unitCoords.second + moveY);
+
+    bool isMoveLegal = false;
+    while (!adjacentSquares.empty())
+    {
+        moveX = adjacentSquares.front().first;
+        moveY = adjacentSquares.front().second;
+        adjacentSquares.pop();
+
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0)
+                    continue;
+
+                if (((moveX + i) + (moveY + j)) >= movesAvaiable)
+                    continue;
+
+                adjacentSquares.emplace(moveX + i, moveY + j);
+            }
+        }
+
+        for (auto enemyUnit : enemyUnits)
+        {
+            auto enemyCoords = enemyUnit.second->get_position();
+            if (enemyCoords.first == moveX && enemyCoords.second == moveY)
+                continue;
+        }
+
+        for (const auto &obstacle : obstacles)
+        {
+            if (obstacle.first == moveX && obstacle.second == moveY)
+                continue;
+        }
+
+        auto enemyBaseCoords = enemyBase->get_position();
+        if (enemyBaseCoords.first == moveX && enemyBaseCoords.second == moveY)
+            continue;
+
+        if (moveX >= mapSize.first || moveY >= mapSize.second || moveX < 0 || moveY < 0)
+            continue;
+
+        legalMoves.emplace_back(((unitCoords.first - moveX) + (unitCoords.second - moveY)), (moveX, moveY));
+    }
+
+    for (auto enemyUnit : enemyUnits)
+    {
+        for (auto &move : legalMoves)
+        {
+            if (enemyUnit.second->attack(move.second.first, move.second.second))
+            {
+                move.first -= fight(enemyUnit.second->get_type(), unit->get_type());
+            }
+        }
+    }
+
+    return std::pair<int, int>{moveX, moveY};
+}
+
 entity *player::create_entity(char unitType, int x, int y)
 {
     entity *newUnit = nullptr;
@@ -118,4 +209,35 @@ entity *player::create_entity(char unitType, int x, int y)
     }
 
     return newUnit;
+}
+
+int player::fight(char attacker, char defender)
+{
+    int damage;
+    switch (attacker)
+    {
+    case 'A':
+        damage = (defender == 'C' || defender == 'R') ? 10 : 15;
+        break;
+    case 'C':
+        damage = (defender == 'B') ? 50 : 40;
+        break;
+    case 'K':
+        damage = (defender == 'R') ? 50 : 35;
+        break;
+    case 'P':
+        damage = (defender == 'B' || defender == 'R') ? 10 : 15;
+        damage = (defender == 'K') ? 35 : damage;
+        break;
+    case 'R':
+        damage = (defender == 'B') ? 50 : 10;
+        break;
+    case 'S':
+        damage = (defender == 'P' || defender == 'C') ? 20 : 30;
+        break;
+    case 'W':
+        damage = (defender == 'B') ? 1 : 5;
+        break;
+    }
+    return damage;
 }
