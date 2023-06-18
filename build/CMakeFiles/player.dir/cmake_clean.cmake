28e58b37a95d6118507fa0ca494de03eb1a378c9
Dawid Lashmann <dawid.lashmann@gmail.com>
2023-06-18 19:33:53 +0200

    update build

---
+++ build/CMakeFiles/player.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/player.dir/entity/entity.cpp.o"
+  "CMakeFiles/player.dir/player/main_player.cpp.o"
+  "CMakeFiles/player.dir/player/player.cpp.o"
+  "player"
+  "player.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/player.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
